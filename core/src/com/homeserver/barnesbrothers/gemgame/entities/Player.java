f3b48eba4775af739d6048f4feb9adf20986519c
David <barnesdavidj@gmail.com>
2015-02-20 12:20:54 -0500

    Add constant value for speed. Replaced hardcoded values with constant.

--- core/src/com/homeserver/barnesbrothers/gemgame/entities/Player.java
+++ core/src/com/homeserver/barnesbrothers/gemgame/entities/Player.java
@@ -8,6 +8,7 @@ import com.homeserver.barnesbrothers.gemgame.handlers.B2DVars;
 
 import static com.homeserver.barnesbrothers.gemgame.handlers.B2DVars.PPM;
 import static com.homeserver.barnesbrothers.gemgame.handlers.B2DVars.SSIZE;
+import static com.homeserver.barnesbrothers.gemgame.handlers.B2DVars.SPEED;
 
 /**
  * Created by david on 2/13/15.
@@ -58,28 +59,28 @@ public class Player extends B2DSprite {
         //Check to see if the player is at one side of the map or the other, and then change direction.
         //if (player.getPosition().x >= GemGame.V_WIDTH / PPM -player.getWidth()/2/PPM) {
         if (this.getPosition().x >= (GemGame.V_WIDTH - (this.getWidth()/2) - (SSIZE*3))/PPM) {
-            this.getBody().setLinearVelocity(-4.0f, 0);
+            this.getBody().setLinearVelocity(-SPEED, 0);
         }
         //if (player.getPosition().x <= 0 + player.getWidth()/2/PPM) {
         if (this.getPosition().x <= ((SSIZE*3) + (this.getWidth()/2))/PPM) {
-            this.getBody().setLinearVelocity(4.0f, 0);
+            this.getBody().setLinearVelocity(SPEED, 0);
         }
 
         //Check to see if the velocity drops bellow 4 in one direction or another, and if so reset it.
-        if (this.getBody().getLinearVelocity().x < 4.0f && this.getBody().getLinearVelocity().x > 0.0f) {
-            this.getBody().setLinearVelocity(4.0f, this.getBody().getLinearVelocity().y);
+        if (this.getBody().getLinearVelocity().x < SPEED && this.getBody().getLinearVelocity().x > 0.0f) {
+            this.getBody().setLinearVelocity(SPEED, this.getBody().getLinearVelocity().y);
         }
-        if (this.getBody().getLinearVelocity().x > -4.0f && this.getBody().getLinearVelocity().x < 0.0f) {
-            this.getBody().setLinearVelocity(-4.0f, this.getBody().getLinearVelocity().y);
+        if (this.getBody().getLinearVelocity().x > -SPEED && this.getBody().getLinearVelocity().x < 0.0f) {
+            this.getBody().setLinearVelocity(-SPEED, this.getBody().getLinearVelocity().y);
         }
 
         //This checks to see if the x velocity has hit zero and then tries to resolve it by making
         //the velocity 4, and setting a bool to check in the next loop. The direction may be the wrong direction
         if (this.getBody().getLinearVelocity().x == 0.0f && !stuckAtZeroV) {
-            this.getBody().setLinearVelocity(4.0f, this.getBody().getLinearVelocity().y);
+            this.getBody().setLinearVelocity(SPEED, this.getBody().getLinearVelocity().y);
             stuckAtZeroV = true;
         } else if(this.getBody().getLinearVelocity().x == 0.0f && stuckAtZeroV) {
-            this.getBody().setLinearVelocity(-4.0f, this.getBody().getLinearVelocity().y);
+            this.getBody().setLinearVelocity(-SPEED, this.getBody().getLinearVelocity().y);
             stuckAtZeroV = false;
         }
     }
