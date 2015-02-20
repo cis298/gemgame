2e314ecc53bf36efa7f99f18071a8a5ee888a071
David <barnesdavidj@gmail.com>
2015-02-20 12:25:06 -0500

    Removed all of the old commented code.

--- core/src/com/homeserver/barnesbrothers/gemgame/states/Play.java
+++ core/src/com/homeserver/barnesbrothers/gemgame/states/Play.java
@@ -54,16 +54,6 @@ public class Play extends GameState {
     private GemManager gemManager;
     private AttunementManager attunementManager;
 
-    //private Array<RedGem> redGems;
-    //private Array<YellowGem> yellowGems;
-    //private Array<GreenGem> greenGems;
-    //private Array<BlueGem> blueGems;
-
-    //private Array<RedAttunement> redAttunments;
-    //private Array<YellowAttunement> yellowAttunments;
-    //private Array<GreenAttunement> greenAttunments;
-    //private Array<BlueAttunement> blueAttunements;
-
     private Array<Spike> spikes;
 
     private boolean stuckAtZeroV;
@@ -83,16 +73,6 @@ public class Play extends GameState {
         gemManager = new GemManager();
         attunementManager = new AttunementManager();
 
-        //redGems = new Array<RedGem>();
-        //yellowGems = new Array<YellowGem>();
-        //greenGems = new Array<GreenGem>();
-        //blueGems = new Array<BlueGem>();
-
-        //redAttunments = new Array<RedAttunement>();
-        //yellowAttunments = new Array<YellowAttunement>();
-        //greenAttunments = new Array<GreenAttunement>();
-        //blueAttunements = new Array<BlueAttunement>();
-
         spikes = new Array<Spike>();
 
         stuckAtZeroV = false;
@@ -103,16 +83,6 @@ public class Play extends GameState {
 
         createPlayer(B2DVars.BIT_PLAYER, playerInteraction, BodyDef.BodyType.DynamicBody);
         createEntity("Exit", B2DVars.BIT_EXIT, B2DVars.BIT_PLAYER, BodyDef.BodyType.StaticBody);
-
-        //createEntities("RedGems", B2DVars.BIT_RED_GEM, B2DVars.BIT_PLAYER, BodyDef.BodyType.StaticBody);
-        //createEntities("YellowGems", B2DVars.BIT_YELLOW_GEM, B2DVars.BIT_PLAYER, BodyDef.BodyType.StaticBody);
-        //createEntities("GreenGems", B2DVars.BIT_GREEN_GEM, B2DVars.BIT_PLAYER, BodyDef.BodyType.StaticBody);
-        //createEntities("BlueGems", B2DVars.BIT_BLUE_GEM, B2DVars.BIT_PLAYER, BodyDef.BodyType.StaticBody);
-
-        //createEntities("RedAttunement", B2DVars.BIT_RED_ATTUNEMENT, B2DVars.BIT_PLAYER, BodyDef.BodyType.StaticBody);
-        //createEntities("YellowAttunement", B2DVars.BIT_YELLOW_ATTUNEMENT, B2DVars.BIT_PLAYER, BodyDef.BodyType.StaticBody);
-        //createEntities("GreenAttunement", B2DVars.BIT_GREEN_ATTUNEMENT, B2DVars.BIT_PLAYER, BodyDef.BodyType.StaticBody);
-        //createEntities("BlueAttunement", B2DVars.BIT_BLUE_ATTUNEMENT, B2DVars.BIT_PLAYER, BodyDef.BodyType.StaticBody);
 
         createEntities("Spikes", B2DVars.BIT_SPIKE, B2DVars.BIT_PLAYER, BodyDef.BodyType.StaticBody);
 
@@ -160,35 +130,6 @@ public class Play extends GameState {
 
         world.step(dt, 6, 2);
 
-        /*
-        //Update all of the Gems
-        for(int i = 0; i < redGems.size; i++) {
-            redGems.get(i).update(dt);
-        }
-        for(int i = 0; i < yellowGems.size; i++) {
-            yellowGems.get(i).update(dt);
-        }
-        for(int i = 0; i < greenGems.size; i++) {
-            greenGems.get(i).update(dt);
-        }
-        for(int i = 0; i < blueGems.size; i++) {
-            blueGems.get(i).update(dt);
-        }
-
-        //Update all of the attunements
-        for(int i = 0; i < redAttunments.size; i++) {
-            redAttunments.get(i).update(dt);
-        }
-        for(int i = 0; i < yellowAttunments.size; i++) {
-            yellowAttunments.get(i).update(dt);
-        }
-        for(int i = 0; i < greenAttunments.size; i++) {
-            greenAttunments.get(i).update(dt);
-        }
-        for(int i = 0; i < blueAttunements.size; i++) {
-            blueAttunements.get(i).update(dt);
-        }
-        */
         for (Map.Entry<String, Array<B2DSprite>> entry : gemManager.getGems().entrySet()) {
             for (B2DSprite gem : entry.getValue()) {
                 gem.update(dt);
@@ -209,78 +150,9 @@ public class Play extends GameState {
         //Update the player and the exit
         player.update(dt);
         exit.update(dt);
-/*
-        //Check to see if the player has no x velocity
-        if (player.getBody().getLinearVelocity().x != 0.0f) {
-            stuckAtZeroV = false;
-        }
 
-        //Check to see if the player is at one side of the map or the other, and then change direction.
-        //if (player.getPosition().x >= GemGame.V_WIDTH / PPM -player.getWidth()/2/PPM) {
-        if (player.getPosition().x >= (GemGame.V_WIDTH - (player.getWidth()/2) - (SSIZE*3))/PPM) {
-            player.getBody().setLinearVelocity(-4.0f, 0);
-        }
-        //if (player.getPosition().x <= 0 + player.getWidth()/2/PPM) {
-        if (player.getPosition().x <= ((SSIZE*3) + (player.getWidth()/2))/PPM) {
-            player.getBody().setLinearVelocity(4.0f, 0);
-        }
-
-        //Check to see if the velocity drops bellow 4 in one direction or another, and if so reset it.
-        if (player.getBody().getLinearVelocity().x < 4.0f && player.getBody().getLinearVelocity().x > 0.0f) {
-            player.getBody().setLinearVelocity(4.0f, player.getBody().getLinearVelocity().y);
-        }
-        if (player.getBody().getLinearVelocity().x > -4.0f && player.getBody().getLinearVelocity().x < 0.0f) {
-            player.getBody().setLinearVelocity(-4.0f, player.getBody().getLinearVelocity().y);
-        }
-
-        //This checks to see if the x velocity has hit zero and then tries to resolve it by making
-        //the velocity 4, and setting a bool to check in the next loop. The direction may be the wrong direction
-        if (player.getBody().getLinearVelocity().x == 0.0f && !stuckAtZeroV) {
-            player.getBody().setLinearVelocity(4.0f, player.getBody().getLinearVelocity().y);
-            stuckAtZeroV = true;
-        } else if(player.getBody().getLinearVelocity().x == 0.0f && stuckAtZeroV) {
-            player.getBody().setLinearVelocity(-4.0f, player.getBody().getLinearVelocity().y);
-            stuckAtZeroV = false;
-        }
-*/
         gemManager.removeGems(world, cl);
-        /*
-        //Remove Gems
-        Array<Body> redGemsToRemove = cl.getRedGemsToRemove();
-        Array<Body> yellowGemsToRemove = cl.getYellowGemsToRemove();
-        Array<Body> greenGemsToRemove = cl.getGreenGemsToRemove();
-        Array<Body> blueGemsToRemove = cl.getBlueGemsToRemove();
 
-        for(int i = 0; i < redGemsToRemove.size; i++) {
-            Body b = redGemsToRemove.get(i);
-            //redGems.removeValue((RedGem) b.getUserData(), true);
-            gemManager.getGems().get("RedGems").removeValue((RedGem) b.getUserData(), true);
-            world.destroyBody(b);
-        }
-        for(int i = 0; i < yellowGemsToRemove.size; i++) {
-            Body b = yellowGemsToRemove.get(i);
-            //yellowGems.removeValue((YellowGem) b.getUserData(), true);
-            gemManager.getGems().get("YellowGems").removeValue((YellowGem) b.getUserData(), true);
-            world.destroyBody(b);
-        }
-        for(int i = 0; i < greenGemsToRemove.size; i++) {
-            Body b = greenGemsToRemove.get(i);
-            //greenGems.removeValue((GreenGem) b.getUserData(), true);
-            gemManager.getGems().get("GreenGems").removeValue((GreenGem) b.getUserData(), true);
-            world.destroyBody(b);
-        }
-        for(int i = 0; i < blueGemsToRemove.size; i++) {
-            Body b = blueGemsToRemove.get(i);
-            //blueGems.removeValue((BlueGem) b.getUserData(), true);
-            gemManager.getGems().get("BlueGems").removeValue((BlueGem) b.getUserData(), true);
-            world.destroyBody(b);
-        }
-
-        redGemsToRemove.clear();
-        yellowGemsToRemove.clear();
-        greenGemsToRemove.clear();
-        blueGemsToRemove.clear();
-*/
         //Level done. Remove exit and player, then start new.
         if (cl.getRemoveExit()) {
             world.destroyBody(exit.getBody());
@@ -295,35 +167,7 @@ public class Play extends GameState {
         tmr.render();
 
         sb.setProjectionMatrix(cam.combined);
-/*
-        //Draw the Gems
-        for(int i = 0; i < redGems.size; i++) {
-            redGems.get(i).render(sb);
-        }
-        for(int i = 0; i < yellowGems.size; i++) {
-            yellowGems.get(i).render(sb);
-        }
-        for(int i = 0; i < greenGems.size; i++) {
-            greenGems.get(i).render(sb);
-        }
-        for(int i = 0; i < blueGems.size; i++) {
-            blueGems.get(i).render(sb);
-        }
 
-        //Draw the attunements
-        for(int i = 0; i < redAttunments.size; i++) {
-            redAttunments.get(i).render(sb);
-        }
-        for(int i = 0; i < yellowAttunments.size; i++) {
-            yellowAttunments.get(i).render(sb);
-        }
-        for(int i = 0; i < greenAttunments.size; i++) {
-            greenAttunments.get(i).render(sb);
-        }
-        for(int i = 0; i < blueAttunements.size; i++) {
-            blueAttunements.get(i).render(sb);
-        }
-*/
         for (Map.Entry<String, Array<B2DSprite>> entry : gemManager.getGems().entrySet()) {
             for (B2DSprite gem : entry.getValue()) {
                 gem.render(sb);
